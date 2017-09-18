b2c1b5b27dbc215ccec63692ad8f3a98b224a46b
coel16 <modopojken@gmail.com>
2017-09-18 10:47:59 +0200

    uppdaterade uppgift6

---
+++ uppgift6.java
@@ -0,0 +1,23 @@
+/*
+En välkomnande kod
+Gjord av Elias Conradsson
+18/9 -17
+*/
+
+import javax.swing.*;
+    
+    public class uppgift6 {
+        public static void main(String[] args) {
+ 
+            
+            char c1;
+            c1 = '\u03c0';
+            char c2;
+            c2 = '\u00F4';
+            char c3;
+            c3 = '\u00D8';
+            JOptionPane.showMessageDialog(null, ""+c1+" "+c2 +" "+c3);
+
+
+        }
+    }
