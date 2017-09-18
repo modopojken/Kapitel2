78fda7d6a057829df683b94ae34f9fd097269d95
coel16 <modopojken@gmail.com>
2017-09-18 08:49:40 +0200

    uppdaterat koden

---
+++ Kapitel2.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+    
+    public class test {
+        public static void main(String[] args) {
+ 
+
+
+cout << "Hello world" << endl;
+
+
+
+        }
+    }
