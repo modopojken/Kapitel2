a03be95d39bab4d9bc51fd9c1bc6e1be2a9f0c17
coel16 <modopojken@gmail.com>
2017-09-18 11:20:54 +0200

    uppdaterat uppgift 2.9

---
+++ uppgift9.java
@@ -0,0 +1,19 @@
+/*
+Uppgift 2.9
+Elias Conradsson
+18/9 -17
+*/
+
+
+import javax.swing.*;
+    
+    public class uppgift6 {
+        public static void main(String[] args) {
+
+        	
+
+
+
+
+      }
+  }
