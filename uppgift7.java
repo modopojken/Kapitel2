bec9aef0d46e45f35e4e7bdbe9e9d1a84336db72
coel16 <modopojken@gmail.com>
2017-09-18 10:49:33 +0200

    uppgift7

---
+++ uppgift7.java
@@ -0,0 +1,21 @@
+/*
+uppgift 2.6 (egentligen 2.6 fast med ett "pi" i. )
+*/
+
+import javax.swing.*;
+    
+    public class uppgift7 {
+        public static void main(String[] args) {
+ 
+            
+            char c1;
+            c1 = '\u03c0';
+            char c2;
+            c2 = '\u00F4';
+            char c3;
+            c3 = '\u00D8';
+            JOptionPane.showMessageDialog(null, ""+c1+" "+c2 +" "+c3);
+
+
+        }
+    }
