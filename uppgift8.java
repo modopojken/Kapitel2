e1ce9e5a9965913cf6abf566422c0952d164582e
coel16 <modopojken@gmail.com>
2017-09-18 11:02:10 +0200

    uppgift 2.8

---
+++ uppgift8.java
@@ -0,0 +1,22 @@
+import javax.swing.*; 
+
+
+/*
+Uppgift 2.8
+Elias Conradsson
+18/9 -17
+
+
+*/
+public class uppgift8 {
+	public static void main(String[]args) {
+
+
+	String a;
+	a= "programspråk";
+	String b;
+	b= a.substring(4,8) + a.charAt(5);
+		JOptionPane.showMessageDialog(null, b);
+
+	}
+}
